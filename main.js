9bbd19d039b095bb7d179ae47ff49b3336df6004
delisha665 <119789223+delisha665@users.noreply.github.com>
2024-06-19 20:19:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+rightWrist_x=0;
+rightWist_y=0;
+leftWrist_x=0;
+leftWrist_y=0;
+
 function preload(){
     hp_sound=loadSound("harry_potter.mp3");
     pp_sound=loadSound("peter_pan.mp3");
@@ -6,9 +11,25 @@ function setup() {
     canvas=createCanvas(400,400);
     canvas.center();
     video=createCapture(VIDEO);
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on("pose",gotResults);
+
     video.hide();
+}
+function modelLoaded(){
+    console.log("success")
 }
 function draw(){
     image(video,0,0,400,400);
 
 }
+function gotResults(results){
+
+    if (results.length>0){
+  leftWrist_x=results[0].pose.leftWrist.x;
+  leftWrist_y=results[0].pose.leftWrist.y;
+  rightWrist_x=results[0].pose.rightWrist.x;
+  rightWrist_y=results[0].pose.rightWrist.y;
+console.log(rightWrist_x)
+    }
+}  
